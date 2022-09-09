c450a966f1834e44403bc0e7b2a18f167f143295
Brian-Momata <brianmomata@gmail.com>
2022-09-09 11:10:57 +0300

    Add Game class with display_board method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,3 +19,19 @@ class Player
     end
   end
 end
+
+class Game
+  @@board = (1..9).to_a
+
+  def Game.display_board
+    row1 = "#{@@board[0]} | #{@@board[1]} | #{@@board[2]}"
+    row2 = "#{@@board[3]} | #{@@board[4]} | #{@@board[5]}"
+    row3 = "#{@@board[6]} | #{@@board[7]} | #{@@board[8]}"
+    separator = '---------'
+    puts row1
+    puts separator
+    puts row2
+    puts separator
+    puts row3
+  end
+end
