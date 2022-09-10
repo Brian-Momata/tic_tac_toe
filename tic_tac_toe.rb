ab59c763773853ca3008766261efb482975c04aa
Brian-Momata <brianmomata@gmail.com>
2022-09-10 08:18:59 +0300

    Add method to check for a winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -66,6 +66,21 @@ class Player < Game
       end
     end
   end
+
+  def is_winner?
+    winning_combos = [[0, 4, 8],
+                     [0, 1, 2], 
+                     [0, 3, 6],
+                     [1, 4, 7], 
+                     [2, 5, 8],
+                     [2, 4, 6],
+                     [3, 4, 5],
+                     [6, 7, 8]
+                    ]
+    winning_combos.any? do |combo|
+      combo.all? { |i| @@board[i] == self.mark}
+    end
+  end
 end
 
 p1 = Player.new
